fd61fd14c067c21ba94f342454ef859265121834
NaoyaMiyagawa <tsuna2764@gmail.com>
2022-05-23 01:22:04 +0900

    feat(caseStudy): add top case design

--- frontend/components/user/pages/case-studies/index.vue
+++ frontend/components/user/pages/case-studies/index.vue
@@ -5,38 +5,48 @@ import LogoSwiper from '../top/parts/LogoSwiper.vue';
 
 const pageTitle = ref('導入事例');
 
+const firstCaseStudy = {
+  id: 1,
+  title: 'Dynamic Markets Orchestrator',
+  company: '清水 Inc',
+  position: 'Chief Directives Consultant',
+  person: '山本 颯太 様',
+  summary: `無駄 弥生 きんく. とちょう みさき 高値 約する 羊毛 すんか きょうどう かわかす くつじょく 抑制. きょうふ
+          げいひんかん 〜亭 やさしい 始まる はんそう. 合う しゃこ 無糖.`,
+};
+
 const caseStudies = [
-  { id: 1, title: '顔を合わせて個別の定義', company: '株式会社 富国商会', position: '人事部', person: '鈴木 敦 様' },
+  { id: 2, title: '顔を合わせて個別の定義', company: '株式会社 富国商会', position: '人事部', person: '鈴木 敦 様' },
   {
-    id: 2,
+    id: 3,
     title: 'リアクティブな新鮮な情報メディア',
     company: '株式会社 首野尾ソリューション',
     position: 'グローバル人材開発室',
     person: '高橋 稔 様',
   },
   {
-    id: 3,
+    id: 4,
     title: '先見の明のある付加価値構造',
     company: '株式会社 笠間商会',
     position: 'チームマネージャー',
     person: '中村 聡 様',
   },
   {
-    id: 4,
+    id: 5,
     title: '互換性のあるクライアントサーバー標準化',
     company: '株式会社 紅葉社',
     position: '労務課',
     person: '渡邉 大介 様',
   },
   {
-    id: 5,
+    id: 6,
     title: '多面的なスケーラブルな投影',
     company: '行政書士法人 GQ社',
     position: '情報システム／総務',
     person: '渡辺 健司 様',
   },
   {
-    id: 6,
+    id: 7,
     title: 'プログラム可能なオブジェクト指向ポータル',
     company: '学校法人 モルサ',
     position: '経営企画部 マネージャー',
@@ -51,12 +61,12 @@ const caseStudies = [
       <div class="p-14 bg-cover bg-[url('https://picsum.photos/id/1049/3900/3120')]">
         <h1 class="text-4xl font-bold text-primary">{{ pageTitle }}</h1>
 
-        <ul class="flex justify-center gap-10 mt-8">
+        <ul class="flex justify-center mt-8 gap-10">
           <NuxtLink to="#case-studies">
-            <li class="p-3 px-8 transition-all border-2 border-black hover:bg-white">導入企業様の声</li>
+            <li class="p-3 px-8 border-2 border-black transition-all hover:bg-white">導入企業様の声</li>
           </NuxtLink>
           <NuxtLink to="#other-companies">
-            <li class="p-3 px-8 transition-all border-2 border-black hover:bg-white">その他の導入企業様</li>
+            <li class="p-3 px-8 border-2 border-black transition-all hover:bg-white">その他の導入企業様</li>
           </NuxtLink>
         </ul>
       </div>
@@ -79,15 +89,11 @@ const caseStudies = [
 
     <div class="p-4">
       <!-- 導入事例 TOP -->
-      <FirstCaseStudy class="p-4 max-w-[80%] m-auto">
-        <template #title>Dynamic Markets Orchestrator</template>
-        <template #companyName>清水 Inc.</template>
-        <template #personInfo>Chief Directives Consultant - 山本 颯太 様</template>
-        <template #description>
-          無駄 弥生 きんく. とちょう みさき 高値 約する 羊毛 すんか きょうどう かわかす くつじょく 抑制. きょうふ
-          げいひんかん 〜亭 やさしい 始まる はんそう. 合う しゃこ 無糖.
-        </template>
-        <template #hoge></template>
+      <FirstCaseStudy :id="firstCaseStudy.id" class="p-4 max-w-[80%] m-auto">
+        <template #title>{{ firstCaseStudy.title }}</template>
+        <template #company>{{ firstCaseStudy.company }}</template>
+        <template #person>{{ firstCaseStudy.position }} - {{ firstCaseStudy.person }}</template>
+        <template #summary>{{ firstCaseStudy.summary }}</template>
       </FirstCaseStudy>
     </div>
 
--- frontend/components/user/pages/case-studies/parts/FirstCaseStudy.vue
+++ frontend/components/user/pages/case-studies/parts/FirstCaseStudy.vue
@@ -1,24 +1,42 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PrimaryBtn from '~~/components/user/buttons/PrimaryBtn.vue';
+const props = defineProps<{ id: number }>();
+</script>
 
 <template>
-  <div class="flex gap-8 text-left">
-    <img src="https://picsum.photos/450/300" width="450" height="300" alt="" />
+  <div>
+    <NuxtLink
+      :to="`/case-studies/view/${$props.id}`"
+      class="group hover:-translate-y-1 flex rounded-xl p-4 gap-8 text-left max-w-[1200px] w-[90%] m-auto hover:shadow-2xl transition-all duration-300 hover:bg-[#13c1aa]"
+    >
+      <img
+        class="rounded-xl transition-all duration-300 group-hover:opacity-80"
+        src="https://picsum.photos/450/300"
+        width="450"
+        height="300"
+        alt=""
+      />
 
-    <div class="">
-      <img src="https://picsum.photos/100" width="100" height="100" alt="" />
+      <div class="group-hover:text-white">
+        <img src="https://picsum.photos/100" width="100" height="100" alt="" />
 
-      <p class="mt-4 text-2xl text-primary">
-        <slot name="title"></slot>
-      </p>
+        <p class="mt-4 text-2xl text-primary group-hover:text-white">
+          <slot name="title"></slot>
+        </p>
 
-      <div class="mt-2 font-bold">
-        <p><slot name="companyName"></slot></p>
-        <span><slot name="personInfo"></slot></span>
+        <div class="mt-2 font-bold">
+          <p><slot name="company"></slot></p>
+          <span><slot name="person"></slot></span>
+        </div>
+
+        <p class="mt-2">
+          <slot name="summary"></slot>
+        </p>
+
+        <div class="mt-4 text-right">
+          <PrimaryBtn>くわしく見る</PrimaryBtn>
+        </div>
       </div>
-
-      <p class="mt-2">
-        <slot name="description"></slot>
-      </p>
-    </div>
+    </NuxtLink>
   </div>
 </template>

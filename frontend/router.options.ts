8cb5493f55d7b2dc6b3999204f14924fdd703109
NaoyaMiyagawa <tsuna2764@gmail.com>
2022-06-19 23:44:20 +0900

    feat(router): add scrollBehavior to scroll top

--- frontend/router.options.ts
+++ frontend/router.options.ts
@@ -2,5 +2,14 @@ import type { RouterConfig } from '@nuxt/schema';
 
 // https://router.vuejs.org/api/#routeroptions
 export default <RouterConfig>{
-  scrollBehavior: {},
+  scrollBehavior(to, from, savedPosition) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          left: savedPosition?.left || 0,
+          top: savedPosition?.top,
+        });
+      }, 0);
+    });
+  },
 };

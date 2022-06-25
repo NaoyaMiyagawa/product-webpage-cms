569d28ade9578c0bc5a8e01126c57290bf28905d
NaoyaMiyagawa <tsuna2764@gmail.com>
2022-06-25 23:49:36 +0900

    refactor(partner): use v-for for text

--- frontend/components/page/partners/index.vue
+++ frontend/components/page/partners/index.vue
@@ -1,6 +1,25 @@
 <script setup lang="ts">
 import { TheContainer, InPageNav } from '~~/components/layout';
 import { LinkMail } from '~~/components/common';
+
+const partnerPlans = [
+  {
+    name: '販売代理店パートナー',
+    content: [
+      'Product を代理販売していただくパートナー制度です。',
+      'クライアント様に当サービスをご紹介いただき、実際のご利用にいたった場合、一定のコミッション費用をお支払いいたします。',
+    ],
+  },
+  {
+    name: 'コンサルティングパートナー',
+    content: [
+      '人事コンサルティングや研修といった人事系・人材育成系サービスを提供されている企業様向けのパートナー制度です。',
+      'パートナー契約を結んでいただくと、貴社のサービスと Product',
+      'を組み合わせて、エンドユーザー様にご提供することが可能になります。',
+      'エンドユーザー様向けの診断ツールをはじめとした、様々な手法でご活用いただけます。',
+    ],
+  },
+];
 </script>
 
 <template>
@@ -13,26 +32,13 @@ import { LinkMail } from '~~/components/common';
         パートナー企業様を２種類のプランで募集しています。
       </p>
 
-      <div class="flex flex-col gap-10 text-left">
-        <div class="mt-8">
-          <p class="text-primary text-3xl underline">販売代理店パートナー</p>
-
-          <p class="mt-4">
-            Product を代理販売していただくパートナー制度です。<br />
-            クライアント様に当サービスをご紹介いただき、実際のご利用にいたった場合、一定のコミッション費用をお支払いいたします。
-          </p>
-        </div>
-
-        <div>
-          <p class="text-primary text-3xl underline">コンサルティングパートナー</p>
-
-          <p class="mt-4">
-            人事コンサルティングや研修といった人事系・人材育成系サービスを提供されている企業様向けのパートナー制度です。<br />
-            パートナー契約を結んでいただくと、貴社のサービスと Product
-            を組み合わせて、エンドユーザー様にご提供することが可能になります。<br />
-            エンドユーザー様向けの診断ツールをはじめとした、様々な手法でご活用いただけます。<br />
-          </p>
-        </div>
+      <div class="flex flex-col gap-10 text-center mt-4 py-4">
+        <template v-for="partnerPlan in partnerPlans" :key="partnerPlan.name">
+          <div>
+            <p class="text-primary text-3xl underline" v-html="partnerPlan.name"></p>
+            <p class="mt-6" v-html="partnerPlan.content.join('<br>')"></p>
+          </div>
+        </template>
       </div>
 
       <div class="text-2xl mt-14">
